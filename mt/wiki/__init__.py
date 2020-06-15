18548e7e659286addf4d36bb5814436206cd80da
Minh-Tri Pham <pmtri80@gmail.com>
2020-06-15 17:33:58 +0100

    more basic files

---
+++ mt/wiki/__init__.py
@@ -0,0 +1,8 @@
+from mt.base import logger, home_dirpath as base_dirpath
+import mt.base.path as _p
+from .version import version as __version__
+
+home_dirpath = _p.join(base_dirpath, 'wiki')
+
+
+__all__ = ['logger', 'home_dirpath']
